152c56a5a24599d647be0196db458e3a3624336a
zaki240 <mzakiputra24@gmail.com>
2022-08-31 08:34:19 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// const txtElement = ['Pelajar',' Pemula' ];
+// let count = 0;
+// let textIndex = 0;
+// let currenTxt = '';
+// let words = '';
+
+let typed = new Typed(".ketikan", {
+  strings: ["  Saya Zaki dan saya adalah seorang Pelajar"],
+  typeSpeed: 200,
+  backDelay: 500,
+  loop: true,
+});
+
+// (function ngetik(){
+
+//     if(count == txtElement.length){
+//         count == 0;
+//     }
+//      currenTxt = txtElement[count];
+
+//      words = currenTxt.slice(0, ++textIndex);
+//      document.querySelector('.ketikan').textContent = words;
+
+//      if(words.length == currenTxt.length){
+//         count++;
+//         textIndex = 0;
+//      }
+
+//      setTimeout(ngetik, 400);
+
+// })();
